d2e1fdee9a43cd308f548314c1dad58336857a70
Stezkoy <stezko@gmail.com>
2026-06-30 15:18:13 +0500

    test

--- src/TelegramServiceProvider.php
+++ src/TelegramServiceProvider.php
@@ -3,16 +3,11 @@
 namespace Stezkoy\NouiTeleAudit;
 
 use Flarum\Foundation\AbstractServiceProvider;
-use Illuminate\Contracts\Config\Repository as ConfigRepository;
 
 class TelegramServiceProvider extends AbstractServiceProvider
 {
     public function register(): void
     {
-        $this->app->singleton(TelegramNotifier::class, function () {
-            return new TelegramNotifier(
-                $this->app->make(ConfigRepository::class)
-            );
-        });
+        $this->container->singleton(TelegramNotifier::class);
     }
 }
